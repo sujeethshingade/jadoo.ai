1c5a69a98d4478d3c9faf16e87ebf3c7fe9ea2f4
perceptronq <hungrydude283@gmail.com>
2024-12-20 22:01:52 +0530

    make vlm callable

--- backend/vlm.py
+++ backend/vlm.py
@@ -1,35 +1,17 @@
 import vertexai
 
-from vertexai.generative_models import GenerativeModel, ChatSession
+from vertexai.generative_models import GenerativeModel, Part
 
-# TODO(developer): Update and un-comment below line
-# PROJECT_ID = "your-project-id"
-vertexai.init(project="jadooai", location="us-central1")
+def generate_description(uri):
+    vertexai.init(project="jadooai", location="us-central1")
 
-model = GenerativeModel("gemini-1.5-flash-002")
+    model = GenerativeModel("gemini-1.5-flash-002")
 
-chat_session = model.start_chat()
+    image_file = Part.from_uri(
+        uri, "image/jpeg"
+    )
 
-def get_chat_response(chat: ChatSession, prompt: str) -> str:
-    text_response = []
-    responses = chat.send_message(prompt, stream=True)
-    for chunk in responses:
-        text_response.append(chunk.text)
-    return "".join(text_response)
-
-prompt = "Hello."
-print(get_chat_response(chat_session, prompt))
-# Example response:
-# Hello there! How can I help you today?
-
-prompt = "What are all the colors in a rainbow?"
-print(get_chat_response(chat_session, prompt))
-# Example response:
-# The colors in a rainbow are often remembered using the acronym ROY G. BIV:
-# * **Red**
-# * **Orange** ...
-
-prompt = "Why does it appear when it rains?"
-print(get_chat_response(chat_session, prompt))
-# Example response:
-# It's important to note that these colors blend smoothly into each other, ...
+    # Query the model
+    response = model.generate_content([image_file, "explain this image in detail"])
+    print(response.text)
+    return response.text
