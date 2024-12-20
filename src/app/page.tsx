66789af03f8007e1cae86b1881cfe8fdeea3e2ef
Sujeeth Shingade <sujeethshingade04@gmail.com>
2024-12-20 17:36:12 +0530

    add Infinite Scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/sections/Navbar";
 import Hero from "@/sections/Hero";
 import Model from "@/sections/Model";
 import Footer from "@/sections/Footer";
+import InfiniteScroll from "@/sections/InfiniteScroll";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <Model />
+      <InfiniteScroll />
       <Footer />
     </div>
   );
