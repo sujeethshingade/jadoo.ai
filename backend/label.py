ad6139dc5dcd816e6efe138b31e8f919222451c8
perceptronq <hungrydude283@gmail.com>
2024-12-20 21:23:29 +0530

    restructure and add backend

---
+++ backend/label.py
@@ -0,0 +1,25 @@
+def detect_labels_uri(uri):
+    """Detects labels in the file located in Google Cloud Storage or on the
+    Web."""
+    from google.cloud import vision
+
+    client = vision.ImageAnnotatorClient()
+    image = vision.Image()
+    image.source.image_uri = uri
+
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    print("Labels:")
+
+    final_labels = []
+    for label in labels:
+        final_labels.append(label.description)
+
+    print(final_labels)
+    if response.error.message:
+        raise Exception(
+            "{}\nFor more info on error messages, check: "
+            "https://cloud.google.com/apis/design/errors".format(response.error.message)
+        )
+    
+    return final_labels
