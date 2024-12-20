c862e4b27c80c870115e3284be058fa00833add8
Sujeeth Shingade <sujeethshingade04@gmail.com>
2024-12-20 21:30:13 +0530

    minor changes

--- src/sections/Navbar.tsx
+++ src/sections/Navbar.tsx
@@ -12,15 +12,15 @@ export default function Header() {
   return (
     <header className="py-4 border-b border-white/15 md:border-none sticky top-0 z-10">
       <div className={`absolute inset-0 backdrop-blur transition-opacity duration-300 ${menuOpen ? 'bg-gray-950 bg-opacity-50' : 'bg-transparent'}`}></div>
-      <div className="container"> 
+      <div className="container">
         <div className="flex justify-between items-center md:border border-white/15 md:p-2.5 max-w-2xl mx-auto rounded-xl relative">
-          <div>
+          <div className="flex items-center gap-2">
             <div className="border w-10 h-10 rounded-lg inline-flex justify-center items-center border-white/15">
               <div className="absolute inset-0 backdrop-blur -z-10 hidden md:block"></div>
               <Image src={LogoIcon.src} alt="Logo" width={32} height={32} className="w-8 h-8" />
             </div>
+            <span className="hidden md:block text-white font-medium">Jadoo.ai</span>
           </div>
-          {/* Menu for large screens */}
           <div className="hidden md:block absolute left-1/2 transform -translate-x-1/2">
             <nav className="flex gap-8 text-sm">
               <a href="#" className="text-white/70 hover:text-white transition">Gallery</a>
@@ -42,7 +42,6 @@ export default function Header() {
           </div>
         </div>
       </div>
-      {/* Sidebar menu for small screens */}
       <div className={`fixed border border-l border-white/15 inset-y-0 right-0 w-80 bg-gray-950 transform ${menuOpen ? 'translate-x-0' : 'translate-x-full'} transition-transform duration-300 ease-in-out z-30 md:hidden`}>
         <div className="flex items-center justify-end p-4">
           <XIcon
