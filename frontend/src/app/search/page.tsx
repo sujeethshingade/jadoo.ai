bfd78fe12ea0579c3274d534c6e27fedc372ddd9
Sujeeth Shingade <sujeethshingade04@gmail.com>
2024-12-21 08:16:00 +0530

    add gallery section

--- frontend/src/app/search/page.tsx
+++ frontend/src/app/search/page.tsx
@@ -2,11 +2,13 @@ import Navbar from "@/sections/Navbar";
 import Footer from "@/sections/Footer";
 import InfiniteScroll from "@/sections/InfiniteScroll";
 import SearchImage from "@/sections/Search";
+import Gallery from "@/sections/Gallery";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Gallery />
       <SearchImage />
       <InfiniteScroll />
       <Footer />
